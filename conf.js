8b64c22031f0a2f262a416b36c3e9eef043a5768
rubbish0815 <rubbish0815@gmail.com>
2017-07-15 19:05:43 +0200

    next

---
+++ conf.js
@@ -0,0 +1,72 @@
+/*jslint node: true */
+"use strict";
+
+exports.clientName = 'byteball';
+exports.minClientVersion = '1.9.1';
+
+// https://console.developers.google.com
+exports.pushApiProjectNumber = 0;
+exports.pushApiKey = '';
+
+exports.port = 6611;
+//exports.myUrl = 'wss://mydomain.com/bb';
+exports.bServeAsHub = true;
+exports.bSaveJointJson = true;
+exports.bLight = false;
+
+// this is used by wallet vendor only, to redirect bug reports to developers' email
+exports.bug_sink_email = 'admin@example.org';
+exports.bugs_from_email = 'bugs@example.org';
+
+exports.HEARTBEAT_TIMEOUT = 300*1000;
+
+exports.storage = 'sqlite';
+
+exports.hub="wss://byteball.org/bb-test";
+exports.deviceName = 'kt-hub-test';
+exports.permanent_paring_secret = '0000';
+exports.KEYS_FILENAME = 'keys.json';
+
+// home wallet (replace these values with the properties of your wallet that is to collect the revenue from sales)
+exports.xPubKey = 'xpub6BnJRgfNeAmgkv42QLAvzSDyWDazuZgp1RLjB33yJCc6vtTs7QmgFDmzwFecaHopsKJhYgNqRbSsPtQjHCXRo591h13mFW7FhhMvqcsT67m';
+exports.account = 6;
+exports.homeDeviceAddress = '0HJBA2MU42AICBCFHPL33H75LWKQ5VCAG';
+
+/* livenet
+exports.initial_witnesses = [
+	'BVVJ2K7ENPZZ3VYZFWQWK7ISPCATFIW3',
+	'DJMMI5JYA5BWQYSXDPRZJVLW3UGL3GJS',
+	'FOPUBEUPBC6YLIQDLKL6EW775BMV7YOH',
+	'GFK3RDAPQLLNCMQEVGGD2KCPZTLSG3HN',
+	'H5EZTQE7ABFH27AUDTQFMZIALANK6RBG',
+	'I2ADHGP4HL6J37NQAD73J7E5SKFIXJOT',
+	'JEDZYC2HMGDBIDQKG3XSTXUSHMCBK725',
+	'JPQKPRI5FMTQRJF4ZZMYZYDQVRD55OTC',
+	'OYW2XTDKSNKGSEZ27LMGNOPJSYIXHBHC',
+	'S7N5FE42F6ONPNDQLCF64E2MGFYKQR2I',
+	'TKT4UESIKTTRALRRLWS4SENSTJX6ODCW',
+	'UENJPVZ7HVHM6QGVGT6MWOJGGRTUTJXQ'
+];
+*/
+
+//testnet
+exports.initial_witnesses = [
+	'2FF7PSL7FYXVU5UIQHCVDTTPUOOG75GX',
+	'2GPBEZTAXKWEXMWCTGZALIZDNWS5B3V7',
+	'4H2AMKF6YO2IWJ5MYWJS3N7Y2YU2T4Z5',
+	'DFVODTYGTS3ILVOQ5MFKJIERH6LGKELP',
+	'ERMF7V2RLCPABMX5AMNGUQBAH4CD5TK4',
+	'F4KHJUCLJKY4JV7M5F754LAJX4EB7M4N',
+	'IOF6PTBDTLSTBS5NWHUSD7I2NHK3BQ2T',
+	'O4K4QILG6VPGTYLRAI2RGYRFJZ7N2Q2O',
+	'OPNUXBRSSQQGHKQNEPD2GLWQYEUY5XLD',
+	'PA4QK46276MJJD5DBOLIBMYKNNXMUVDP',
+	'RJDYXC4YQ4AZKFYTJVCR5GQJF5J6KPRI',
+	'WELOXP3EOA75JWNO6S5ZJHOO3EYFKPIR'
+];
+
+exports.initial_peers = [
+	'wss://byteball.org/bb-test'
+];
+
+console.log('finished hub conf');
---
+++ start.js
@@ -0,0 +1,170 @@
+/*jslint node: true */
+"use strict";
+require('byteball-relay');
+var conf = require('byteballcore/conf.js');
+var network = require('byteballcore/network');
+var eventBus = require('byteballcore/event_bus.js');
+var push = require('./push');
+
+var fs = require('fs');
+var crypto = require('crypto');
+var util = require('util');
+var device = require('byteballcore/device.js');
+var db = require('byteballcore/db.js');
+var walletDefinedByKeys = require('byteballcore/wallet_defined_by_keys.js');
+var desktopApp = require('byteballcore/desktop_app.js');
+
+
+var appDataDir = desktopApp.getAppDataDir();
+var KEYS_FILENAME = appDataDir + '/' + conf.KEYS_FILENAME;
+
+var wallet;
+
+
+function replaceConsoleLog(){
+	var log_filename = conf.LOG_FILENAME || (appDataDir + '/log.txt');
+	var writeStream = fs.createWriteStream(log_filename);
+	console.log('---------------');
+	console.log('From this point, output will be redirected to '+log_filename);
+	console.log("To release the terminal, type Ctrl-Z, then 'bg'");
+	console.log = function(){
+		writeStream.write(Date().toString()+': ');
+		writeStream.write(util.format.apply(null, arguments) + '\n');
+	};
+	console.warn = console.log;
+	console.info = console.log;
+}
+
+function isControlAddress(device_address){
+	return (conf.control_addresses && conf.control_addresses.indexOf(device_address) >= 0);
+}
+
+function readKeys(onDone){
+	fs.readFile(KEYS_FILENAME, 'utf8', function(err, data){
+		if (err){
+			console.log('failed to read keys, will gen');
+			var devicePrivKey = crypto.randomBytes(32);
+			var deviceTempPrivKey = crypto.randomBytes(32);
+			var devicePrevTempPrivKey = crypto.randomBytes(32);
+			writeKeys(devicePrivKey, deviceTempPrivKey, devicePrevTempPrivKey, function(){
+				onDone(devicePrivKey, deviceTempPrivKey, devicePrevTempPrivKey);
+			});
+			return;
+		}
+		var keys = JSON.parse(data);
+		onDone(Buffer(keys.permanent_priv_key, 'base64'), Buffer(keys.temp_priv_key, 'base64'), Buffer(keys.prev_temp_priv_key, 'base64'));
+	});
+}
+
+function writeKeys(devicePrivKey, deviceTempPrivKey, devicePrevTempPrivKey, onDone){
+	var keys = {
+		permanent_priv_key: devicePrivKey.toString('base64'),
+		temp_priv_key: deviceTempPrivKey.toString('base64'),
+		prev_temp_priv_key: devicePrevTempPrivKey.toString('base64')
+	};
+	fs.writeFile(KEYS_FILENAME, JSON.stringify(keys), 'utf8', function(err){
+		if (err)
+			throw Error("failed to write keys file");
+		if (onDone)
+			onDone();
+	});
+}
+
+function createWallet(onDone){
+	walletDefinedByKeys.createSinglesigWalletWithExternalPrivateKey(conf.xPubKey, conf.account, conf.homeDeviceAddress, function(_wallet){
+		wallet = _wallet;
+		onDone();
+	});
+}
+
+function handleNoWallet(from_address){
+	if (from_address === conf.homeDeviceAddress && wallet === null)
+		createWallet(function(){
+			device.sendMessageToDevice(from_address, 'text', "Wallet created, all new addresses will be synced to your device");
+		});
+	else
+		device.sendMessageToDevice(from_address, 'text', "The Hub Wallet is not setup yet, try again later");
+}
+
+function compareVersions(currentVersion, minVersion) {
+	if (currentVersion === minVersion) return '==';
+
+	var cV = currentVersion.match(/([0-9])+/g);
+	var mV = minVersion.match(/([0-9])+/g);
+	var l = Math.min(cV.length, mV.length);
+	var diff;
+
+	for (var i = 0; i < l; i++) {
+		diff = parseInt(cV[i], 10) - parseInt(mV[i], 10);
+		if (diff > 0) {
+			return '>';
+		} else if (diff < 0) {
+			return '<'
+		}
+	}
+
+	diff = cV.length - mV.length;
+	if (diff == 0) {
+		return '==';
+	} else if (diff > 0) {
+		return '>';
+	} else if (diff < 0) {
+		return '<';
+	}
+}
+
+
+
+eventBus.on('peer_version', function (ws, body) {
+	if (body.program == conf.clientName) {
+		if (conf.minClientVersion && compareVersions(body.program_version, conf.minClientVersion) == '<')
+			network.sendJustsaying(ws, 'new_version', {version: conf.minClientVersion});
+		if (compareVersions(body.program_version, '1.5.1') == '<')
+			ws.close(1000, "mandatory upgrade");
+	}
+});
+
+eventBus.on('paired', function(from_address){
+	if (!wallet)
+		return handleNoWallet(from_address);
+	device.sendMessageToDevice(from_address, 'text', "A Hub with a Merchant say's hello.");
+});
+
+eventBus.on('text', function(from_address, text){
+	if (!wallet)
+		return handleNoWallet(from_address);
+	text = text.trim().toLowerCase();
+});
+
+
+replaceConsoleLog();
+
+if (!conf.permanent_paring_secret)
+	throw Error('no conf.permanent_paring_secret');
+db.query(
+	"INSERT "+db.getIgnore()+" INTO pairing_secrets (pairing_secret, expiry_date, is_permanent) VALUES(?, '2035-01-01', 1)", 
+	[conf.permanent_paring_secret]
+);
+
+db.query("SELECT wallet FROM wallets", function(rows){
+	if (rows.length > 1)
+		throw Error('more than 1 wallet');
+	if (rows.length === 1)
+		wallet = rows[0].wallet;
+	else
+		wallet = null; // different from undefined
+});
+	
+
+readKeys(function(devicePrivKey, deviceTempPrivKey, devicePrevTempPrivKey){
+	var saveTempKeys = function(new_temp_key, new_prev_temp_key, onDone){
+		writeKeys(devicePrivKey, new_temp_key, new_prev_temp_key, onDone);
+	};
+	device.setDevicePrivateKey(devicePrivKey);
+	device.setTempKeys(deviceTempPrivKey, devicePrevTempPrivKey, saveTempKeys);
+	device.setDeviceName(conf.deviceName);
+	device.setDeviceHub(conf.myhub);
+	var my_device_pubkey = device.getMyDevicePubKey();
+	console.log("my device pubkey: "+my_device_pubkey);
+	console.log("my pairing code: "+my_device_pubkey+"@"+conf.hub+"#"+conf.permanent_paring_secret);
+});
